003e90544d16da9b41515ce3c30d6da9452ee2db
JasonChristo99 <38409798+JasonChristo99@users.noreply.github.com>
2020-12-07 11:37:21 +0200

    exported components as dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,11 @@ import 'materialize-css/dist/css/materialize.min.css';
 export {default  as  Button} from './Button';
 export {default  as  Example} from './Example';
 export {default  as  Card} from './Card';
+export {default  as  Icon} from './Icon';
+export {default  as  Nav} from './Nav';
+export {default  as  NavLinks} from './NavLinks';
+export {default  as  NavLink} from './NavLink';
+export {default  as  NavLogo} from './NavLogo';
 
 //Helpers
 export {default  as  Row} from './Row';
