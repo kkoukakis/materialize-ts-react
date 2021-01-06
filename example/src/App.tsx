2d47d253a00613a4bbd194d897490223a5f7605a
JasonChristo99 <38409798+JasonChristo99@users.noreply.github.com>
2021-01-06 15:02:43 +0200

    select, option, optgroup

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,47 +1,85 @@
 import React from 'react'
 
-import {Button, Card, Row, Container, Icon, Nav, NavLinks, NavLink, NavLogo} from 'materialize-ts-react'
+import {
+  Button,
+  Card,
+  Row,
+  Container,
+  Icon,
+  Nav,
+  NavLinks,
+  NavLink,
+  NavLogo,
+  Select,
+  Option,
+  OptGroup
+} from 'materialize-ts-react'
 
 import './index.css'
- 
+
 const App = () => {
-  return <>
-       <Container>
-      
-      <h1>Row</h1>
+  return (
+    <>
+      <Container>
+        <h1>Row</h1>
 
-      <Row>
-      
-          <h5>
-            I am a row
-          </h5>   
+        <Row>
+          <h5>I am a row</h5>
+        </Row>
+
+        <h1>Button</h1>
+
+        <Button
+          id='mybutton'
+          text='hey'
+          onClick={() => {
+            alert('clicked something')
+          }}
+        />
+
+        <h1>Card</h1>
+
+        <Card id='1' text='kalhmera' card_title='title' />
+
+        <h1>Icon</h1>
+
+        <Icon iconCode='edit' />
+
+        <h1>Nav</h1>
+
+        <Nav>
+          <NavLogo text='Brand Logo' alignment='left' />
+          <NavLinks alignment='right'>
+            <NavLink title='First link' />
+            <NavLink title='Second link' />
+          </NavLinks>
+        </Nav>
+
+        <h1>Select</h1>
+
+        <h2>Options</h2>
+
+        <Select>
+          <Option value='1' text='First' />
+          <Option value='2' text='Second' />
+          <Option value='3' text='Third' disabled />
+        </Select>
+
+        <h2>Option Groups</h2>
         
-      </Row>
-
-      <h1>Button</h1>
-
-       <Button id="mybutton" text="hey" onClick={()=>{alert('clicked something')}}/>
-
-      <h1>Card</h1>
-
-       <Card id="1" text="kalhmera" card_title="title"/>
-
-      <h1>Icon</h1>
-
-       <Icon iconCode="edit" />
-      
-      <h1>Nav</h1>
-
-       <Nav>
-         <NavLogo text="Brand Logo" alignment="left" />
-         <NavLinks alignment="right">
-           <NavLink title="First link" />
-           <NavLink title="Second link" />
-         </NavLinks>
-       </Nav>
-
-       </Container>
-  </>
+        <Select>
+          <OptGroup>
+            <Option value='1' text='First' />
+            <Option value='2' text='Second' />
+          </OptGroup>
+          <OptGroup>
+            <Option value='3' text='Third' disabled />
+            <Option value='4' text='Fourth' disabled />
+          </OptGroup>
+        </Select>
+      </Container>
+    </>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,23 @@
-import 'materialize-css/dist/css/materialize.min.css';
- 
-//Components
-export {default  as  Button} from './Button';
-export {default  as  Card} from './Card';
-export {default  as  Icon} from './Icon';
-export {default  as  Nav} from './Nav';
-export {default  as  NavLinks} from './NavLinks';
-export {default  as  NavLink} from './NavLink';
-export {default  as  NavLogo} from './NavLogo';
+import 'materialize-css/dist/css/materialize.min.css'
 
-//Helpers
-export {default  as  Row} from './Row';
-export {default  as  Container} from './Container'; 
-export {default  as  Hide} from './Hide'; 
+// Components
+export { default as Button } from './Button'
+export { default as Card } from './Card'
+export { default as Icon } from './Icon'
+export { default as Nav } from './Nav'
+export { default as NavLinks } from './NavLinks'
+export { default as NavLink } from './NavLink'
+export { default as NavLogo } from './NavLogo'
+export { default as Select } from './Select'
+export { default as Option } from './Option'
+export { default as OptGroup } from './OptGroup'
 
-//Active Components
+// Helpers
+export { default as Row } from './Row'
+export { default as Container } from './Container'
+export { default as Hide } from './Hide'
 
-//Forms
-export {default  as  Switch} from './Switch'; 
+// Active Components
+
+// Forms
+export { default as Switch } from './Switch'
